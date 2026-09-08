8af7b67ec9b95587856655cae968ee4cad6e4665
YUSUKE IZAWA <me@yizawa.com>
2026-09-08 19:37:20 +0200

    Example config: Ghostty terminal, spacing 5, ThreeColMid and Circle layouts

--- config/xmonad.hs
+++ config/xmonad.hs
@@ -2,19 +2,23 @@ import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns()
 import XMonad.MacOS
 
 main :: IO ()
 main = xmonad $ def
-  { terminal = "open -a Terminal"
+  { terminal = "open -a Ghostty"
   -- M = Option. Option-letter no longer types accented characters for bound
   -- keys; use mod4Mask for Command or controlMask .|. mod4Mask for both.
   , modMask = mod1Mask
   -- Ten workspaces: M-1..M-9 then M-0; M-S-N moves the focused window.
   , workspaces = map show ([1..9] :: [Int]) ++ ["0"]
   -- spacing 4 means a four-point inset on each window edge (eight between tiles).
-  , layoutHook = spacing 4 $
-      Tall 1 (3/100) (1/2) ||| Mirror (Tall 1 (3/100) (1/2)) ||| Full
+  , layoutHook = spacing 5 $
+      Tall 1 (3/100) (1/2)
+      ||| ThreeColMid 1 (3/100) (1/2)     -- ThreeColMid for a centred master
+      ||| Circle
+      ||| Full
   , manageHook = composeAll
       [ bundleId =? "com.apple.systempreferences" --> doFloat
       -- , bundleId =? "com.mitchellh.ghostty" --> doShift "2"
